a056a5fdd6dd9456229b72e49e794fd547493c30
cm <connormccarthy8@gmail.com>
2018-10-12 12:27:48 -0700

    finish basic specs and start adding interface.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+import { SuperGalacticAgeCalculator } from "./SuperGalacticAgeCalculator.js";
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
 
 $(document).ready(function() {
+  $("#formDate").submit(function(event) {
+    event.preventDefault();
+    let birthday = $("#birthday").val();
+    let age = $("#age").val();
 
+    let calc = new SuperGalacticAgeCalculator(birthday, age);
+
+    $("#mercuryAge").text(calc.ageOnMercury());
+    $("#venusAge").text(calc.ageOnVenus());
+    $("#marsAge").text(calc.ageOnMars());
+    $("#jupiterAge").text(calc.ageOnJupiter());
+  });
 });
