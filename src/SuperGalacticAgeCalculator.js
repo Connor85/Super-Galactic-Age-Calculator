731c64c42293a87669cf6218aef4f3a7d240a3b0
cm <connormccarthy8@gmail.com>
2018-10-13 10:09:56 -0700

    update methods to calculate age on other planets based off users birthday

--- src/SuperGalacticAgeCalculator.js
+++ src/SuperGalacticAgeCalculator.js
@@ -11,51 +11,52 @@ export class SuperGalacticAgeCalculator {
   }
 
   ageOnEarth() {
-    let earthYears = this.age;
+    const secodsAYear = 31557600;
+    let earthYears = Math.round(this.ageInSeconds() / secodsAYear);
     return earthYears;
   }
 
   ageOnMercury() {
-    let mercuryYears = Math.round(this.age / 0.24);
+    let mercuryYears = Math.round(this.ageOnEarth() / 0.24);
     return mercuryYears;
   }
 
   ageOnVenus() {
-    let venusYears = Math.round(this.age / 0.62);
+    let venusYears = Math.round(this.ageOnEarth() / 0.62);
     return venusYears;
   }
 
   ageOnMars() {
-    let marsYears = Math.round(this.age / 1.88);
+    let marsYears = Math.round(this.ageOnEarth() / 1.88);
     return marsYears;
   }
 
   ageOnJupiter() {
-    let jupiterYears = Math.round(this.age / 11.86);
+    let jupiterYears = Math.round(this.ageOnEarth() / 11.86);
     return jupiterYears;
   }
 
   yearsLeftOnEarth() {
-    let yearsOnEarth = this.lifeExpectancy - this.age;
+    let yearsOnEarth = this.lifeExpectancy - this.ageOnEarth();
     return yearsOnEarth;
   }
 
   yearsLeftOnMercury() {
-    let yearsOnMercury = (this.lifeExpectancy - this.age) / 0.24;
+    let yearsOnMercury = (this.lifeExpectancy - this.ageOnEarth()) / 0.24;
     return yearsOnMercury;
   }
 
   yearsLeftOnVenus() {
-    let yearsOnVenus = Math.round((this.lifeExpectancy - this.age) / 0.62);
+    let yearsOnVenus = Math.round((this.lifeExpectancy - this.ageOnEarth()) / 0.62);
     return yearsOnVenus;
   }
 
   yearsLeftOnMars() {
-    let yearsOnMars = Math.round((this.lifeExpectancy - this.age) / 1.88);
+    let yearsOnMars = Math.round((this.lifeExpectancy - this.ageOnEarth()) / 1.88);
     return yearsOnMars;
   }
   yearsLeftOnJupiter() {
-    let yearsOnJupiter = Math.round((this.lifeExpectancy - this.age) / 11.86);
+    let yearsOnJupiter = Math.round((this.lifeExpectancy - this.ageOnEarth()) / 11.86);
     return yearsOnJupiter;
   }
 }
